1026cf0ee50d30b1def3cbfc037873fbdf61be00
Bkhbou <76_telex.cadet@icloud.com>
2025-06-29 20:47:42 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pour Toi ❤️", page_icon="💖", layout="centered")
+
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1 style="color: #ff4b6e;">Bienvenue mon amour 💕</h1>
+        <p style="font-size: 20px;">Ce site a été créé juste pour toi, pour te montrer combien je t’aime. 💌</p>
+        <img src="https://i.imgur.com/Z5cU3uG.png" width="200">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
