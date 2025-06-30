8d31d8c568a043283a8e1a958add884081db22ab
Bkhbou <76_telex.cadet@icloud.com>
2025-06-30 03:38:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ elif menu == "📸 Galerie":
     if not os.path.exists(photos_dir):
         st.warning("Ajoute les photos 2, 3, 4 dans le dossier `assets/photos/`")
     else:
-         photos = ["2.JPG", "3.JPG", "4.JPG"]
+         photos = ["2.jpg", "3.jpg", "4.JPG","5.JPG"]
         for photo in photos:
             path = os.path.join(photos_dir, photo)
             if os.path.exists(path):
